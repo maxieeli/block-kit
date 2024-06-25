ec7cdb31ba7f37c1b10e89304085b853ec52617a
maxiee <owenlideveloper@gmail.com>
2024-06-26 07:37:01 +0800

    feat: update global env and expection folder

---
+++ packages/framework/global/src/exceptions/code.ts
@@ -0,0 +1,10 @@
+export enum ErrorCode {
+  DefaultRuntimeError = 1,
+  ValueNotExists,
+  ValueNotInstanceOf,
+  ValueNotEqual,
+  MigrationError,
+  SchemaValidateError,
+  // Fatal error should be greater than 10000
+  DefaultFatalError = 10000,
+}
---
+++ packages/framework/global/src/exceptions/index.ts
@@ -0,0 +1,30 @@
+import type { ErrorCode } from "./code.js";
+
+export class BlockKitError extends Error {
+  code: ErrorCode;
+  isFatal: boolean;
+  constructor(code: ErrorCode, message: string) {
+    super(message);
+    this.name = 'BlockSuiteError';
+    this.code = code;
+    this.isFatal = code >= 10000;
+  }
+}
+
+export function handleError(error: Error) {
+  if (!(error instanceof BlockKitError)) {
+    throw error;
+  }
+  if (error.isFatal) {
+    throw new Error(
+      'A fatal error for BlockSuite occurs, please contact the team if you find this.',
+      { cause: error }
+    );
+  }
+  console.error(
+    "A runtime error for BlockSuite occurs, you can ignore this error if it won't break the user experience."
+  );
+  console.error(error.stack);
+}
+
+export * from './code.js';
