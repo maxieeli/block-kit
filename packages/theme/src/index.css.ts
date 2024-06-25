93b69ee88deb600bf7d9b35a731b595fbe2fcc50
maxiee <owenlideveloper@gmail.com>
2024-06-26 07:21:09 +0800

    feat: update theme package

---
+++ packages/theme/src/index.css.ts
@@ -0,0 +1,14 @@
+import { globalStyle } from '@vanilla-extract/css';
+import { darkCssVariables, lightCssVariables } from './index';
+
+globalStyle(':root', {
+  vars: lightCssVariables,
+});
+
+globalStyle('[data-theme="light"]', {
+  vars: lightCssVariables,
+});
+
+globalStyle('[data-theme="dark"]', {
+  vars: darkCssVariables,
+});
