626df5bb8c6f581e83b792098cf44e8ba847d25c
maxiee <owenlideveloper@gmail.com>
2024-06-26 07:32:29 +0800

    feat: update global package

---
+++ packages/framework/global/index.d.ts
@@ -0,0 +1,14 @@
+declare type BlockSuiteFlags = {
+  enable_synced_doc_block: boolean;
+  enable_pie_menu: boolean;
+  enable_database_statistics: boolean;
+  enable_database_attachment_note: boolean;
+  enable_block_query: boolean;
+  enable_legacy_validation: boolean;
+  enable_expand_database_block: boolean;
+  enable_lasso_tool: boolean;
+  enable_edgeless_text: boolean;
+  enable_ai_onboarding: boolean;
+  readonly: Record<string, boolean>;
+};
+  
