af54b6ec13058665bb329cffcafaff068a4bd531
maxiee <owenlideveloper@gmail.com>
2024-07-03 07:11:19 +0800

    feat: update inline query

---
+++ packages/framework/inline/src/utils/query.ts
@@ -0,0 +1,17 @@
+import { assertExists } from '@maxiee/block_global/utils';
+
+import { INLINE_ROOT_ATTR } from '../consts.js';
+import type { InlineEditor, InlineRootElement } from '../inline-editor.js';
+
+export function getInlineEditorInsideRoot(element: Element): InlineEditor {
+  const rootElement = element.closest(
+    `[${INLINE_ROOT_ATTR}]`
+  ) as InlineRootElement;
+  assertExists(rootElement, 'element must be inside a v-root');
+  const inlineEditor = rootElement.inlineEditor;
+  assertExists(
+    inlineEditor,
+    'element must be inside a v-root with inline-editor'
+  );
+  return inlineEditor;
+}
