5a049deebd71cb7cd8afaf1fe0c7d6eb3eae83e7
maxiee <owenlideveloper@gmail.com>
2024-07-14 17:10:27 +0800

    feat: update store transformer

---
+++ packages/framework/store/src/transformer/slice.ts
@@ -0,0 +1,49 @@
+import { assertExists } from '@maxiee/block_global/utils';
+import type { Doc } from '../store/index.js';
+import type { DraftModel } from './draft.js';
+
+type SliceData = {
+  content: DraftModel[];
+  workspaceId: string;
+  pageId: string;
+  pageVersion: number;
+  workspaceVersion: number;
+};
+
+export class Slice {
+  get content() {
+    return this.data.content;
+  }
+
+  get pageVersion() {
+    return this.data.pageVersion;
+  }
+
+  get workspaceVersion() {
+    return this.data.workspaceVersion;
+  }
+
+  get workspaceId() {
+    return this.data.workspaceId;
+  }
+
+  get docId() {
+    return this.data.pageId;
+  }
+
+  constructor(readonly data: SliceData) {}
+
+  static fromModels(doc: Doc, models: DraftModel[]) {
+    const meta = doc.collection.meta;
+    const { pageVersion, workspaceVersion } = meta;
+    assertExists(pageVersion);
+    assertExists(workspaceVersion);
+    return new Slice({
+      content: models,
+      workspaceId: doc.collection.id,
+      pageId: doc.id,
+      pageVersion,
+      workspaceVersion,
+    });
+  }
+}
