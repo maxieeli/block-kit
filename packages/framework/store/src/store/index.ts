54d8eb84a23fbdb9a2b9c1af17fecb4d575a38e2
maxiee <owenlideveloper@gmail.com>
2024-07-14 15:13:27 +0800

    feat: add store package

---
+++ packages/framework/store/src/store/index.ts
@@ -0,0 +1,7 @@
+export type * from './collection.js';
+export { DocCollection } from './collection.js';
+export type * from './doc/block-collection.js';
+export * from './doc/index.js';
+export type * from './meta.js';
+export * from './space.js';
+export * from './store.js';
